76719e5b483baec067218a0fba74a24f884e842d
Adam-Mickiewicz <marketing@nadwyraz.com>
2026-03-22 22:50:29 +0000

    chore: refresh materialized views after truncate

---
+++ supabase/migrations/030_refresh_views.sql
@@ -0,0 +1,9 @@
+-- Odśwież wszystkie widoki zmaterializowane po TRUNCATE
+REFRESH MATERIALIZED VIEW crm_overview;
+REFRESH MATERIALIZED VIEW crm_segments;
+REFRESH MATERIALIZED VIEW crm_risk;
+REFRESH MATERIALIZED VIEW crm_worlds;
+REFRESH MATERIALIZED VIEW crm_occasions;
+REFRESH MATERIALIZED VIEW crm_behavior_segments;
+REFRESH MATERIALIZED VIEW crm_cohorts;
+REFRESH MATERIALIZED VIEW crm_segment_worlds;
